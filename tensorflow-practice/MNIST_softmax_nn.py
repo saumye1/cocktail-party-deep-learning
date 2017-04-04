1cd8e07fe46af7d758d62344c0d82eeb4925a212
Arun Mittal <arunmittal53@users.noreply.github.com>
2017-04-04 17:01:09 +0100

    Update MNIST_softmax_nn.py

--- tensorflow-practice/MNIST_softmax_nn.py
+++ tensorflow-practice/MNIST_softmax_nn.py
@@ -23,16 +23,16 @@ def neural_network_model(data):
 	# (input_data * weights) + biases
 
 	hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-					  'biases':tf.Variable(tf.random_normal(n_nodes_hl1))}
+					  'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
 	hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-					  'biases':tf.Variable(tf.random_normal(n_nodes_hl2))}
+					  'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
 	hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl13])),
-					  'biases':tf.Variable(tf.random_normal(n_nodes_hl3))}
+					  'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
 	output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-					'biases':tf.Variable(tf.random_normal(n_classes))}
+					'biases':tf.Variable(tf.random_normal([n_classes]))}
 
 	l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
 	l1 = tf.nn.relu(l1)
@@ -50,7 +50,7 @@ def neural_network_model(data):
 
 def train_neural_network(x):
 	prediction = neural_network_model(x)
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y))
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=prediction ,logits=y))
 	optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 	hm_epoches = 10
